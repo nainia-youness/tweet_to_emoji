4b4fdbf446aa18396b8edcd5efbac67234718bbf
unknown <nainiayouness@gmail.com>
2020-12-03 14:39:12 +0100

    API working and app running

--- Client/src/Components/Page.js
+++ Client/src/Components/Page.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {InputGroup,FormControl,Container,Row, Col,Button} from 'react-bootstrap'
-
+import {InputGroup,FormControl,Container,Row, Col,Button,Card} from 'react-bootstrap'
+import axios from 'axios'
+import ClipLoader from "react-spinners/ClipLoader";
 
 class  Page extends Component{
   constructor(props) {
     super(props)
     this.state={
         tweet:'',
-        emoji:''
+        emoji:'',
+        loading: false,
     }
     this.handler = this.handler.bind(this);
 }
@@ -20,9 +22,34 @@ myChangeHandler = (event) => {
     });
   }
 
+unicodeToChar(text) {
+    return text.replace(/\\u[\dA-F]{4}/gi, 
+           function (match) {
+                return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+           });
+ }
+ 
 
 handler(){
-    console.log('Predict')
+    this.setState({
+        loading:true
+    })
+    axios.post('/api',{tweet:this.state.tweet})
+                .then(res=>{
+                    this.setState({
+                        emoji:this.unicodeToChar(res.data.emoji)
+                    })
+                    console.log(res)
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+                .finally(()=>
+                {
+                    this.setState({
+                        loading:false
+                    })
+                })
 }
 
 render() {
@@ -59,10 +86,32 @@ render() {
                     <Col style={{height:'20px'}}></Col>
                 </Row>
                 <Row>
+                  <Col>
+                        <ClipLoader textAlign='center'
+                            size={100}
+                            color={"blue"}
+                            loading={this.state.loading}
+                        />
+                  </Col>
+              </Row>
+                {this.state.emoji!==''  &&
+                 <Row  style={{textAlign:'center'}}>
+                    <Col style={{ width: '18rem' }}></Col>
                     <Col>
-                        {this.state.emoji}
+                        <Card
+                            bg='Primary'
+                            style={{ width: '18rem' }}
+                            className="mb-2"
+                        >
+                            <Card.Header>Emoji</Card.Header>
+                            <Card.Body>
+                            <Card.Title ><span style={{fontSize:'35px'}}>{this.state.emoji}</span></Card.Title>
+                            </Card.Body>
+                        </Card>
                     </Col>
+                    <Col style={{ width: '18rem' }}></Col>
                 </Row>
+                }
             </Container>
 
         </>
